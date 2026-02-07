d8ebd2e797ef9de137b8bcacb527d460d91bb823
edesta25 <157256768+edesta25@users.noreply.github.com>
2026-02-06 21:50:01 -0600

    Implement RSA key pair generation and expiration check

---
+++ keys.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/hex"
+	"time"
+)
+
+type KeyPair struct {
+	KID     string
+	Expires time.Time
+	Priv    *rsa.PrivateKey
+	Pub     *rsa.PublicKey
+}
+
+func GenerateRSAKeyPair(bits int, expires time.Time) (*KeyPair, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a random kid (16 bytes hex) - good enough for assignment
+	kidBytes := make([]byte, 16)
+	if _, err := rand.Read(kidBytes); err != nil {
+		return nil, err
+	}
+
+	return &KeyPair{
+		KID:     hex.EncodeToString(kidBytes),
+		Expires: expires,
+		Priv:    priv,
+		Pub:     &priv.PublicKey,
+	}, nil
+}
+
+func (k *KeyPair) IsExpired(now time.Time) bool {
+	return !k.Expires.After(now)
+}
