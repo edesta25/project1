b2b001fd3f76f5f39f2414a0c02d7555264bbfcc
edesta25 <157256768+edesta25@users.noreply.github.com>
2026-02-06 22:00:42 -0600

    Add Service struct and logging middleware
    Removed comments about key expiration in NewService function.

--- server.go
+++ server.go
@@ -12,13 +12,11 @@ type Service struct {
 }
 
 func NewService() (*Service, error) {
-	// Active key expires in 24 hours
 	active, err := GenerateRSAKeyPair(2048, time.Now().Add(24*time.Hour))
 	if err != nil {
 		return nil, err
 	}
 
-	// Expired key expired 24 hours ago
 	expired, err := GenerateRSAKeyPair(2048, time.Now().Add(-24*time.Hour))
 	if err != nil {
 		return nil, err
